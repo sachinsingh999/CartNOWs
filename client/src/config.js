00f7e5c773b73a07622e1334311a03c4a04b3fd5
sachin kumar <sachinkumar@Mac-1.lan>
2026-07-05 21:31:49 +0530

    fix: updated calling alorithms

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+export const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://cartnows.onrender.com";
